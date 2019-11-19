f4502118e7c4ec2e5b20b7a9def1a01845a863db
Céline Merlet <celine.merlet.fr@gmail.com>
2019-11-19 17:33:09 +0100

    Add files via upload

---
+++ resid-time-acf.c
@@ -0,0 +1,304 @@
+/* This program calculates residence time autocorrelation functions to evaluate exchange kinetics
+   in and out of a defined region. */
+
+/* Inspired from the methodology described in:
+S. A. Kislenko - R. H. Amirov - I. S. Samoylov 
+Phys. Chem. Chem. Phys., 12, 11245 (2010) */
+
+/* 
+Intermittent function = <hI(t).hI(0)>/<hI(0)^2>
+Continuous function = <hC(t).hC(0)>/<hC(0)^2>
+*/
+
+/* To execute the program, you just have to launch it with an input file giving
+the following information (program < inputfile)
+first line: name of the xyz file with the positions (> name)
+second line: number of configurations in this file (> nconfigs)
+third line: sampling parameter for configurations, i.e. configurations are considered every nskip (> nskip)
+(if nskip = 1, all configurations are considered)
+fourth line: time between two considered configurations (> dtime)
+fifth line: number of species for which the calculation will be done (> nspecies) !MAX 1000!
+next lines: atom type for each species (> atomtype)
+next line: limits in the z direction (> zmin,zmax)
+next line: number of correlations steps (> ncorr)
+*/
+
+/* !ONLY WORKS FOR A CONSTANT NUMBER OF ATOMS WRITTEN IN THE SAME ORDER FOR ALL CONFIGURATIONS! */
+/* Note that no conversion is done so the time unit in the output is the same as the input. */ 
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#define L 1000
+
+
+char name[50];
+/* Possibility to have up to 1000 atom types */
+char atomtype[1001][5],type[5];
+int **normI, **normC, nconfigs, nspecies, nionstot, ncorr, nskip;
+double zmin, zmax;
+double **storeC, **cfC, dstoreC, rvacfC, dtime, prodstoreC;
+double **storeI, **cfI, dstoreI, rvacfI;
+
+
+void read();
+void calculate();
+void write();
+
+/* For dynamic allocation */
+int **imatrix(int nl, int nc);
+double **dmatrix(int nl, int nc);
+
+
+int main ( void )
+{
+ printf("\nHello!\n\n");
+ read();
+ calculate();
+ write();
+ printf("\nDone!\n\n");
+
+ return 0;
+}
+
+
+void read()
+{
+ int i,j,count;
+ FILE *in;
+ char ligne[L];
+
+ /* Reads the information in the input file */
+ scanf("%s",name);		/* name of the file with the positions */
+ scanf("%d",&nconfigs);		/* number of configurations in the file */
+ scanf("%d",&nskip);		/* number of skips between considered configurations */
+ scanf("%lf",&dtime);		/* time between two considered configurations in ps */
+ scanf("%d",&nspecies);		/* number of species for which the calculation will be done */
+ for(i=1;i<=nspecies;i++)
+    {
+     scanf("%s",atomtype[i]);   /* atom type for each species */
+    }
+ scanf("%lf %lf",&zmin,&zmax);	/* limits in the z direction */
+ scanf("%d",&ncorr);		/* number of correlations steps */
+
+ /* Read the total number of atoms in the xyz file */
+ in=fopen(name,"r");
+ fgets(ligne,L,in);
+ sscanf(ligne,"%d",&nionstot);
+ fclose(in);
+  
+ printf("Total number of atoms: %d.\n",nionstot);
+
+ /* Allocates the matrices */
+ storeI=dmatrix(nionstot+1,ncorr+1); 
+ storeC=dmatrix(nionstot+1,ncorr+1); 
+ cfI=dmatrix(ncorr+1,nspecies+1); normI=imatrix(ncorr+1,nspecies+1);
+ cfC=dmatrix(ncorr+1,nspecies+1); normC=imatrix(ncorr+1,nspecies+1);
+}
+
+
+void calculate()
+{
+ int i,ii,iii,j,k,l,n,itype;
+ double z;
+ FILE *in;
+ char ligne[L];
+
+ in=fopen(name,"r"); 
+
+ /* Calculation of the autocorrelation function */
+ for(i=1;i<=nconfigs;i++)
+    {
+     /* Skip the line with the number of atoms */
+     fgets(ligne,L,in);
+     /* Skip the comment line */
+     fgets(ligne,L,in);
+     if((i%100)==0) printf("%d\n",i);
+     if((i%nskip)==0) 
+       {
+        ii=i/nskip;
+        if(ii<=ncorr) 
+          {
+           for(j=1;j<=nionstot;j++) 
+	      {	
+    	       /* Get the atom type and z position for each atom */
+   	       fgets(ligne,L,in);
+     	       sscanf(ligne,"%s %*lf %*lf %lf",type,&z);
+	       /* Check if the atom is considered in this calculation */
+	       itype=0;
+	       for(l=1;l<=nspecies;l++)
+	          {
+	           if(strcmp(atomtype[l],type)==0) itype=l;
+	          }
+	       if(itype!=0)
+	         {
+	          /* Main calculation: heaviside and autocorrelation */
+	          if((z<=zmax)&&(z>=zmin)) 
+	            {
+		     storeI[j][ii]=1; storeC[j][ii]=1;
+		    }
+	          if((z>zmax)||(z<zmin)) 
+	            {
+	             storeI[j][ii]=0; storeC[j][ii]=0;
+	            }
+	          dstoreI=storeI[j][ii]; 
+	          dstoreC=storeC[j][ii];
+	          for(k=ii;k>=1;k--) 
+	             {
+	              rvacfI=dstoreI*storeI[j][k]; cfI[ii-k][itype]+=rvacfI; 
+                      normI[ii-k][itype]++;
+		      prodstoreC=storeC[j][k];
+                      n=k+1;
+		      while((n<ii)&&(prodstoreC==1))
+		           {
+		            prodstoreC*=storeC[j][n];
+		            n++;
+		           }
+		      rvacfC=dstoreC*prodstoreC; cfC[ii-k][itype]+=rvacfC; 
+                      normC[ii-k][itype]++;
+	             }
+	         }
+      	      }
+           }
+       if(ii>ncorr) 
+         {
+          iii=ii%ncorr;
+          for(j=1;j<=nionstot;j++) 
+    	     {
+	     /* Get the atom type and z position for each atom */
+	     fgets(ligne,L,in);
+	     sscanf(ligne,"%s %*lf %*lf %lf",type,&z);
+	     /* Check if the atom is considered in this calculation */
+	     itype=0;
+	     for(l=1;l<=nspecies;l++)
+	        {
+	         if(strcmp(atomtype[l],type)==0) itype=l;
+	        }
+	     if(itype!=0)
+	       {
+	        /* Main calculation: heaviside and autocorrelation */
+	        if((z<=zmax)&&(z>=zmin)) 
+	          {
+	           storeI[j][iii]=1; storeC[j][iii]=1;
+	          }
+	        if((z>zmax)||(z<zmin)) 
+	          {
+	           storeI[j][iii]=0; storeC[j][iii]=0;
+	          }
+	        dstoreI=storeI[j][iii];
+                dstoreC=storeC[j][iii];
+	        for(k=ncorr;k>=iii+1;k--) 
+	           {
+	            rvacfI=dstoreI*storeI[j][k]; cfI[iii-k+ncorr][itype]+=rvacfI; 
+                    normI[iii-k+ncorr][itype]++;
+	            prodstoreC=storeC[j][k];
+	            n=k+1;
+	            while((n<=ncorr)&&(prodstoreC==1))
+	    	         {
+		    	  prodstoreC*=storeC[j][n];
+			  n++;
+		         }
+		    n=1;
+		    while((n<iii)&&(prodstoreC==1))
+		         {
+		   	  prodstoreC*=storeC[j][n];
+			  n++;
+		         }
+		    rvacfC=dstoreC*prodstoreC; cfC[iii-k+ncorr][itype]+=rvacfC; 
+                    normC[iii-k+ncorr][itype]++;
+		   }
+	        for(k=iii;k>=1;k--) 
+	           {
+	            rvacfI=dstoreI*storeI[j][k]; cfI[iii-k][itype]+=rvacfI; 
+		    normI[iii-k][itype]++;
+		    prodstoreC=storeC[j][k];
+                    for(n=k+1;n<iii;n++) 
+		       {
+		        if(n>iii) printf("Problem\n"); 
+		        prodstoreC*=storeC[j][n];
+		       }
+		    rvacfC=dstoreC*prodstoreC; cfC[iii-k][itype]+=rvacfC; 
+ 		    normC[iii-k][itype]++;
+		   }		    
+	        }
+             } /* end loop nionstot */
+          } /* end if i/ncorr */
+       } /* end if i/nskip */
+      else	
+       {
+       for(j=1;j<=nionstot;j++) 
+          {	
+           /* Get the atom type and z position for each atom */
+           fgets(ligne,L,in);
+           sscanf(ligne,"%s %*lf %*lf %lf",type,&z);
+          }	
+       }
+    } /* end loop on configs */
+
+ /* Normalisation of the autocorrelation function */
+ for(i=0;i<ncorr;i++)
+  {
+    for(j=1;j<=nspecies;j++)
+       {
+        cfI[i][j]/=(normI[i][j]*1.0);
+        cfC[i][j]/=(normC[i][j]*1.0);
+       }
+  }
+
+ fclose(in);
+
+
+}
+
+void write()
+{
+ int i,j;
+ FILE *out;
+
+ for(j=1;j<=nspecies;j++)
+    {
+     sprintf(name,"%s%d.dat","resid-time-acf",j);
+     out=fopen(name,"w");
+     fprintf(out,"# Time - Intermittent function - Continuous function\n");
+     for(i=0;i<ncorr;i++) 
+	{
+	 fprintf(out,"%lf	%lf	%lf\n",i*dtime,cfI[i][j]/cfI[0][j],cfC[i][j]/cfC[0][j]);
+	}
+     fclose(out); 
+    }
+
+}
+
+
+/********************************************************************************************/
+/*Allocation dynamique*/
+
+int **imatrix(int nl, int nc)
+{
+  int i;
+  int **m;
+  m=(int **) malloc(nl*sizeof(int*));
+  if (m) { m[0]=(int *) malloc(nl*nc*sizeof(int));
+           if (m[0]==NULL) return NULL;
+           for (i=1;i<nl;i++) m[i]=m[i-1]+nc;
+         }
+  return m;
+}
+
+double **dmatrix(int nl, int nc)
+{
+  int i;
+  double **m;
+  m=(double **) malloc(nl*sizeof(double*));
+  if (m) { m[0]=(double *) malloc(nl*nc*sizeof(double));
+           if (m[0]==NULL) return NULL;
+           for (i=1;i<nl;i++) m[i]=m[i-1]+nc;
+         }
+  return m;
+}
+
+
+
+
